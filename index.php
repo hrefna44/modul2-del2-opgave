15675a938e6ce15afdc6d76d54362e6ddcd9e596
Sophie <sophiehboye@hotmail.com>
2016-09-28 11:10:09 +0200

    oprettelse af dokumenter

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <title>Welcome to your website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+</head>
+
+<body>
+
+<h1>Please login or register</h1>    
+    
+</body>
+</html>
